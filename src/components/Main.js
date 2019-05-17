1215c7fbee781c97227cc932122b1637728cee0c
yujin19 <35741231+yujin19@users.noreply.github.com>
2019-05-16 20:13:06 -0700

    add register

---
+++ src/components/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Register } from "./Register";
+
+export class Main extends React.Component {
+  render() {
+    return <Register />;
+  }
+}
---
+++ src/components/Register.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export class Register extends React.Component {
+  render() {
+    return <div>Register</div>;
+  }
+}
